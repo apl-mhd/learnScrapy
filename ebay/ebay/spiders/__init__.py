d6579ad05892701679f04fbe6d7a681603954e76
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-07-15 01:51:52 +0600

    ebay website scrapin updateg

--- ebay/ebay/spiders/__init__.py
+++ ebay/ebay/spiders/__init__.py
@@ -15,37 +15,53 @@ class ebay(scrapy.Spider):
 
                   ]
 
+    url=''
+
 #response.css('li.sresult.lvresult')
     def parse(self, response):
         for i in response.css('li.sresult.lvresult'):
 
+            ebay.url = i.css('a.vip::attr(href)').get()
 
-            yield{
-
-                'product_name': i.css("a.vip::text").get(),
-                'product_url': i.css('a.vip::attr(href)').get(),
-                'img_url':i.css('img.img::attr(src)').get(),
-                'price':i.css('li.lvprice.prc span::text').get(),
-                
-
-            }
+            yield scrapy.Request(ebay.url, callback=self.productPage)
 
 
         next_page = response.css('a.gspr.next::attr(href)').get()
 
         if next_page is not None:
-            yield scrapy.Request(url= next_page, callback=self.parse)
+            yield scrapy.Request(url= next_page, callback=self.parse)    
 
-'''
-            next_page = response.selector.xpath("//a[@class='next_page']/@href").extract_first()
 
-            if next_page is not None:
+    def productPage(self, response):
 
-                next_page_link = response.urljoin(next_page)
 
-                yield scrapy.Request(url=next_page_link, callback=self.parse)
+        yield{
+                'product_name': response.css("h1.it-ttl::text").get(),
+                'sold': response.css("a.vi-txt-underline::text").get(),
+                'price': response.css("span#prcIsum::attr('content')").get(),
+                'item_number': response.css("div#descItemNumber::text").get(),
+                'img': response.css("img#icImg::attr('src')").get(),
+                'product_url':ebay.url 
 
-'''
+            }     
+
+
+#
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''
 
             loader = ItemLoader(item=QuoteItem(), selector=quote, response=response)
@@ -57,4 +73,22 @@ class ebay(scrapy.Spider):
             yield  loader.load_item()
 
 
+'''
+
+
+
+
+
+
+'''
+            yield{
+
+                'product_name': i.css("a.vip::text").get(),
+                'product_url': i.css('a.vip::attr(href)').get(),
+                'img_url':i.css('img.img::attr(src)').get(),
+                'price':i.css('li.lvprice.prc span::text').get(),
+                
+
+            }
+            
 '''
