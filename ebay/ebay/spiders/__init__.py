751a12d850578587e863079af79028f480084b77
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2019-07-15 16:36:48 +0600

    all most done

--- ebay/ebay/spiders/__init__.py
+++ ebay/ebay/spiders/__init__.py
@@ -34,46 +34,27 @@ class ebay(scrapy.Spider):
 
     def productPage(self, response):
 
-        trs = response.css('div.itemAttr tr')   
+        trs = response.css('div.itemAttr tr')
 
-        a={}         
-
+        a={} 
     
-        x = trs[0].css('td::text')[0].get()
-        y = str(trs[0].css('td')[1].css('span#vi-cond-addl-info::text').get()) + str(trs[0].css('td')[1].css('span#vi-cond-addl-info::text').get()) 
-        a[x] = y
-
-        x = 'Brand'
-        y = trs[0].css('td')[3].css('span::text').get()
-        a[x] = y
-
-
-        x = trs[1].css('td::text')[0].get()
-        y = trs[1].css('td')[1].css('span::text').get()
-        a[x]=y
-
-        x = trs[1].css('td::text')[2].get()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for i in trs:
         
+            x = i.css('td::text')[0].get().replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '').replace('\n\t\t\t\t\t\t\t\t\t \t\t\t','')
+            y = i.css('td')[1].css('span::text').get().replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '').replace('\n\t\t\t\t\t\t\t\t\t \t\t\t', '')
+            a[x]= y    
 
 
+            x=None
+            y=None
 
 
-                     
-        
+            x = i.css('td::text')[2].get().replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '').replace('\n\t\t\t\t\t\t\t\t\t \t\t\t', '')
+            if x is not None:
+                y = i.css('td')[3].css('span::text').get().replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '')
+                a[x]= y 
+            
+
 
         yield{
                 'product_name': response.css("h1.it-ttl::text").get(),
@@ -92,49 +73,3 @@ class ebay(scrapy.Spider):
              }     
 
 
-#
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-            loader = ItemLoader(item=QuoteItem(), selector=quote, response=response)
-
-            loader.add_xpath('text', ".//div[@class='quoteText']/text()")
-            loader.add_xpath('author', ".//span[@class='authorOrTitle']/text()")
-            loader.add_xpath('tags', ".//div[@class ='greyText smallText left']/a/text()")
-
-            yield  loader.load_item()
-
-
-'''
-
-
-
-
-
-
-'''
-            yield{
-
-                'product_name': i.css("a.vip::text").get(),
-                'product_url': i.css('a.vip::attr(href)').get(),
-                'img_url':i.css('img.img::attr(src)').get(),
-                'price':i.css('li.lvprice.prc span::text').get(),
-                
-
-            }
-            
-'''
